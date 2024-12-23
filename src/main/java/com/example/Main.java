1353c45891421acad341f218a66f1ea756943ddc
Nahuel Lemes <nahulem@gmail.com>
2024-12-22 22:54:06 -0300

    refactor(main): refactorizacion del Main

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import com.example.controllers.ChatController;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        ChatController controller = new ChatController();
+        controller.start();
     }
 }
